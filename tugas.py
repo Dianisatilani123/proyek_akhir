4b3a7c718b4c327c89b11a4499ec8a1c4587ca84
DianisaTilani <92613189+Dianisatilani123@users.noreply.github.com>
2024-06-04 10:26:30 +0800

    Update tugas.py

--- tugas.py
+++ tugas.py
@@ -83,7 +83,10 @@ relevent_experience = st.selectbox("Relevent Experience", label_encoders['releve
 enrolled_university = st.selectbox("Enrolled University", label_encoders['enrolled_university'].classes_)
 education_level = st.selectbox("Education Level", label_encoders['education_level'].classes_)
 major_discipline = st.selectbox("Major Discipline", label_encoders['major_discipline'].classes_)
-experience = st.selectbox("Experience", ['<1', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '>20'])
+
+experience_mapping = {'<1': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, '11': 11, '12': 12, '13': 13, '14': 14, '15': 15, '16': 16, '17': 17, '18': 18, '19': 19, '20': 20, '>20': 21}
+experience = st.selectbox("Experience", list(experience_mapping.keys()))
+
 company_size = st.selectbox("Company Size", label_encoders['company_size'].classes_)
 company_type = st.selectbox("Company Type", label_encoders['company_type'].classes_)
 last_new_job = st.selectbox("Last New Job", label_encoders['last_new_job'].classes_)
@@ -99,7 +102,7 @@ if st.button("Predict"):
         'enrolled_university': enrolled_university,
         'education_level': education_level,
         'major_discipline': major_discipline,
-        'experience': experience,
+        'experience': experience_mapping[experience],
         'company_size': company_size,
         'company_type': company_type,
         'last_new_job': last_new_job,
