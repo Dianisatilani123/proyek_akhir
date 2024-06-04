6988b9ef95c473adf6f22d54ae7eba64d1361f66
DianisaTilani <92613189+Dianisatilani123@users.noreply.github.com>
2024-06-04 12:18:19 +0800

    Update tugas.py

--- tugas.py
+++ tugas.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import accuracy_score, classification_report, f1_score
 import joblib
 import streamlit as st
 
