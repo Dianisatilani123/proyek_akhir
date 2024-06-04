a39c3d21b27e8dabea0f97a74822a04a5efacd9c
DianisaTilani <92613189+Dianisatilani123@users.noreply.github.com>
2024-06-04 10:24:27 +0800

    Update tugas.py

--- tugas.py
+++ tugas.py
@@ -43,7 +43,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 # 3. Melatih model
 model = RandomForestClassifier(n_estimators=100, random_state=42)
-model.fit(X_train, y_train)
+model.fit(X_train.values, y_train.values)
 
 # 4. Membuat prediksi
 def predict(data):
