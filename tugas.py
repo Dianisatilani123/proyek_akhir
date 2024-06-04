c2d7a638cfa77e106e2947269fcca957e4a7077b
DianisaTilani <92613189+Dianisatilani123@users.noreply.github.com>
2024-06-04 21:42:49 +0800

    Update tugas.py

--- tugas.py
+++ tugas.py
@@ -4,7 +4,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix
 from sklearn.preprocessing import LabelEncoder
-import matplotlib.pyplot as plt
 import streamlit as st
 
 # Load dataset
