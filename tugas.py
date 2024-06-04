dab5af632b5cf6f6afb69aa94c09d26e872cf4eb
DianisaTilani <92613189+Dianisatilani123@users.noreply.github.com>
2024-06-04 12:14:36 +0800

    Update tugas.py

--- tugas.py
+++ tugas.py
@@ -1,3 +1,11 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
+import joblib
+import streamlit as st
+
 # Load dataset
 df = pd.read_csv('aug_train.csv')
 
