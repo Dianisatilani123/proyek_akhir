27183ce359ffde005b5f1dd08a067583b5551f3e
DianisaTilani <92613189+Dianisatilani123@users.noreply.github.com>
2024-06-05 06:41:52 +0800

    Update tugas.py

--- tugas.py
+++ tugas.py
@@ -16,10 +16,10 @@ print(df.head())
 df.drop(['enrollee_id', 'city', 'gender'], axis=1, inplace=True)  # remove columns that can cause bias
 
 # Fill missing values in categorical columns with a placeholder
-df.loc[df['relevent_experience'].isna(), 'relevent_experience'] = 'Unknown'
+df.loc[df['relevent_experience'].isna(), 'elevent_experience'] = 'Unknown'
 df.loc[df['enrolled_university'].isna(), 'enrolled_university'] = 'Unknown'
 df.loc[df['education_level'].isna(), 'education_level'] = 'Unknown'
-df.loc[df['major_discipline'].isna(), 'major_discipline'] = 'Unknown'
+df.loc[df['major_discipline'].isna(), 'ajor_discipline'] = 'Unknown'
 df.loc[df['company_size'].isna(), 'company_size'] = 'Unknown'
 df.loc[df['company_type'].isna(), 'company_type'] = 'Unknown'
 df.loc[df['last_new_job'].isna(), 'last_new_job'] = 'Unknown'
@@ -48,13 +48,13 @@ possible_company_type = ['Pvt Ltd', 'Funded Startup', 'Public Sector', 'Unknown'
 possible_last_new_job = ['never', '>4', '>2', '>1', '<1', 'Unknown']
 
 # Fit LabelEncoders with predefined categories
-le_relevent_experience = LabelEncoder().fit(df['relevent_experience'])
-le_enrolled_university = LabelEncoder().fit(df['enrolled_university'])
-le_education_level = LabelEncoder().fit(df['education_level'])
-le_major_discipline = LabelEncoder().fit(df['major_discipline'])
-le_company_size = LabelEncoder().fit(df['company_size'])
-le_company_type = LabelEncoder().fit(df['company_type'])
-le_last_new_job = LabelEncoder().fit(df['last_new_job'])
+le_relevent_experience = LabelEncoder().fit(possible_relevent_experience)
+le_enrolled_university = LabelEncoder().fit(possible_enrolled_university)
+le_education_level = LabelEncoder().fit(possible_education_level)
+le_major_discipline = LabelEncoder().fit(possible_major_discipline)
+le_company_size = LabelEncoder().fit(possible_company_size)
+le_company_type = LabelEncoder().fit(possible_company_type)
+le_last_new_job = LabelEncoder().fit(possible_last_new_job)
 
 df['relevent_experience'] = le_relevent_experience.transform(df['relevent_experience'])
 df['enrolled_university'] = le_enrolled_university.transform(df['enrolled_university'])
@@ -64,7 +64,7 @@ df['company_size'] = le_company_size.transform(df['company_size'])
 df['company_type'] = le_company_type.transform(df['company_type'])
 df['last_new_job'] = le_last_new_job.transform(df['last_new_job'])
 
-# Create a new feature that combines 'relevent_experience' and 'experience'
+# Create a new feature that combines 'elevent_experience' and 'experience'
 df['experience_score'] = df['relevent_experience'] * df['experience']
 
 # Split data into training and testing sets
@@ -100,33 +100,18 @@ company_type = st.selectbox("Company Type", possible_company_type)
 last_new_job = st.selectbox("Last New Job", possible_last_new_job)
 training_hours = st.slider("Training Hours", 0, 100, 20)
 
-# Check if user-entered company_size is in the list of predefined categories
-possible_company_size = ['<1', '1-49', '50-99', '100-500', '500-999', 'Unknown']
-le_company_size = LabelEncoder().fit(possible_company_size)
-
-input_data = pd.DataFrame({'city_development_index': [city_development_index],
-                           'relevent_experience': [relevent_experience],
-                           'enrolled_university': [enrolled_university],
-                           'education_level': [education_level],
-                           'major_discipline': [major_discipline],
-                           'experience': [experience],
-                           'company_size': [company_size],
-                           'company_type': [company_type],
-                           'last_new_job': [last_new_job],
-                           'training_hours': [training_hours],
-                           'experience_score': [0]})  # Added 'experience_score' column
-
 # Convert categorical data into numerical data using the same LabelEncoders
-input_data['relevent_experience'] = le_relevent_experience.transform([relevent_experience])
-input_data['enrolled_university'] = le_enrolled_university.transform([enrolled_university])
-input_data['education_level'] = le_education_level.transform([education_level])
-input_data['major_discipline'] = le_major_discipline.transform([major_discipline])
-input_data['company_size'] = le_company_size.transform([company_size])
-input_data['company_type'] = le_company_type.transform([company_type])
-input_data['last_new_job'] = le_last_new_job.transform([last_new_job])
-
-# Calculate 'experience_score' for input data
-input_data['experience_score'] = input_data['relevent_experience'] * input_data['experience']
+input_data = pd.DataFrame({'city_development_index': [city_development_index],
+                           'elevent_experience': [le_relevent_experience.transform([relevent_experience])[0]],
+                           'enrolled_university': [le_enrolled_university.transform([enrolled_university])[0]],
+                           'education_level': [le_education_level.transform([education_level])[0]],
+                           'ajor_discipline': [le_major_discipline.transform([major_discipline])[0]],
+                           'experience': [experience],
+                           'company_size': [le_company_size.transform([company_size])[0]],
+                           'company_type': [le_company_type.transform([company_type])[0]],
+                           'last_new_job': [le_last_new_job.transform([last_new_job])[0]],
+                           'training_hours': [training_hours],
+                           'experience_score': [le_relevent_experience.transform([relevent_experience])[0] * experience]})
 
 # Check if company_size is None before making a prediction
 if company_size is not None:
