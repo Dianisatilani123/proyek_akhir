89efabd7aa033e2d39b3b875f41cb8128c8d52b0
DianisaTilani <92613189+Dianisatilani123@users.noreply.github.com>
2024-06-05 19:15:37 +0800

    Update tugas2.py

--- tugas2.py
+++ tugas2.py
@@ -22,10 +22,12 @@ def preprocess_data(data):
     categorical_cols = ['relevent_experience', 'enrolled_university', 'education_level', 
                         'ajor_discipline', 'company_size', 'company_type', 'last_new_job']
     for col in categorical_cols:
-        data[col] = label_encoder.fit_transform(data[col])
+        if col in data.columns:
+            data[col] = label_encoder.fit_transform(data[col])
+        else:
+            print(f"Column '{col}' does not exist in the dataset.")
 
     return data
-
 # Langkah 4: Split data train dan test
 def split_data(data):
     X = data.drop(columns=["gender", "city"])  # Hapus fitur "City"
