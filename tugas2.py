ed275c760e440622a8d8e2af7929a11ef6a47e58
DianisaTilani <92613189+Dianisatilani123@users.noreply.github.com>
2024-06-06 09:16:58 +0800

    Update tugas2.py

--- tugas2.py
+++ tugas2.py
@@ -4,7 +4,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from sklearn.preprocessing import LabelEncoder
-import base64 
 
 # Langkah 2: Load dataset
 def load_data():
