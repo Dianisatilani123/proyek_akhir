109857dcc7393fd7875cfa43237dbdcdc35aec36
DianisaTilani <92613189+Dianisatilani123@users.noreply.github.com>
2024-06-09 19:46:41 +0800

    Update tugas2.py

--- tugas2.py
+++ tugas2.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+from streamlit import beta_columns
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
