4d5fae66b28390d76df3d4ac52f255f4c3aeef16
DianisaTilani <92613189+Dianisatilani123@users.noreply.github.com>
2024-06-10 19:42:41 +0800

    Update tugas2.py

--- tugas2.py
+++ tugas2.py
@@ -99,8 +99,10 @@ def login():
         if username == "admin" and password == "admin123":
             st.session_state['logged_in'] = True
             st.success("Login berhasil!")
+            st.experimental_rerun()  # Refresh halaman setelah login berhasil
         else:
             st.error("Username atau Password salah!")
+            st.experimental_rerun()  # Refresh halaman setelah logout berhasil
 
 # Tombol logout
 def logout():
