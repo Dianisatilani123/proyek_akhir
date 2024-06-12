fddb9dfc0cb160bc722721cb50d3b78d38ac44b6
DianisaTilani <92613189+Dianisatilani123@users.noreply.github.com>
2024-06-12 09:31:34 +0800

    Update tugas2.py

--- tugas2.py
+++ tugas2.py
@@ -237,26 +237,19 @@ def export_report_to_pdf(data, gender_counts, education_counts, experience_count
     for last_new_job, count in last_new_job_counts.items():
         pdf.cell(200, 10, txt=f"{last_new_job}: {count}", ln=True)
 
-    # Menyimpan file PDF
+    # Menyimpan grafik sebagai gambar dan menambahkannya ke PDF
+    for i, fig in enumerate(figures, start=1):
+        img_path = f"figure_{i}.png"
+        fig.savefig(img_path)
+        pdf.add_page()
+        pdf.image(img_path, x=10, y=10, w=pdf.w - 20)
+
     pdf_file = "Laporan_Keberagaman.pdf"
     pdf.output(pdf_file)
-
+    
     return pdf_file
 
-# Fungsi untuk mengubah file menjadi format base64
-def file_to_base64(file_path):
-    with open(file_path, "rb") as file:
-        file_content = file.read()
-    return base64.b64encode(file_content).decode()
-
-# Langkah 8: Membuat sistem prediksi
-def predict_candidate(model, candidate_data):
-    prediction = model.predict(candidate_data)
-    probability = model.predict_proba(candidate_data)
-
-    return prediction[0], probability.max()
-
-# Ekspor hasil prediksi ke PDF dengan warna teks
+# Ekspor hasil prediksi ke PDF
 def export_prediction_to_pdf(prediction_data):
     pdf = FPDF()
     pdf.add_page()
@@ -265,15 +258,6 @@ def export_prediction_to_pdf(prediction_data):
     # Menambahkan konten ke PDF
     pdf.cell(200, 10, txt="Hasil Prediksi Kandidat", ln=True, align='C')
     for key, value in prediction_data.items():
-        # Tentukan warna teks berdasarkan keterangan
-        if key == "Keterangan":
-            if value == "Kandidat Ditolak":
-                pdf.set_text_color(255, 0, 0)  # Merah untuk kandidat ditolak
-            elif value == "Kandidat Diterima":
-                pdf.set_text_color(0, 255, 0)  # Hijau untuk kandidat diterima
-        else:
-            pdf.set_text_color(0, 0, 0)  # Hitam untuk teks lainnya
-
         pdf.cell(200, 10, txt=f"{key}: {value}", ln=True)
 
     pdf_file = "Hasil_Prediksi.pdf"
@@ -281,110 +265,184 @@ def export_prediction_to_pdf(prediction_data):
 
     return pdf_file
 
-# Streamlit app
+# Fungsi untuk menampilkan tautan unduhan
+def download_file(file_path):
+    with open(file_path, "rb") as file:
+        btn = st.download_button(
+            label="Download Laporan",
+            data=file,
+            file_name=file_path,
+            mime="application/octet-stream"
+        )
+        return btn
+
+# Halaman login
+def login():
+    st.markdown("<h2>Login Admin</h2>", unsafe_allow_html=True)
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+    if st.button("Login"):
+        if username == "admin" and password == "admin123":
+            st.session_state['logged_in'] = True
+            st.success("Login berhasil!")
+            st.experimental_rerun()  # Refresh halaman setelah login berhasil
+        else:
+            st.error("Username atau Password salah!")
+            st.set_option('deprecation.showPyplotGlobalUse', False)  # Disable the warning
+           
+# Tombol logout
+def logout():
+    if st.button("Logout"):
+        st.session_state['logged_in'] = False
+        st.success("Logout berhasil!")
+        st.experimental_rerun()  # Refresh halaman setelah logout berhasil
+
+# Fungsi untuk memvalidasi dataset yang diunggah
+def validate_input(data):
+    if data is None:
+        st.error("File tidak diunggah atau dalam format yang salah.")
+        return False
+    # Tambahkan validasi tambahan sesuai kebutuhan
+    return True
+
+def save_model(model, file_path="model.pkl"):
+    try:
+        joblib.dump(model, file_path)
+        st.success("Model berhasil disimpan!")
+    except Exception as e:
+        st.error(f"Gagal menyimpan model: {str(e)}")
+
 def main():
-    st.title("Aplikasi Analitik dan Prediksi Kandidat")
+    st.markdown("<h1 style='text-align: center'>Aplikasi Rekrutmen Tanpa Bias Gender</h1>", unsafe_allow_html=True)
+    add_custom_css()  # Tambahkan CSS khusus untuk tombol
 
-    add_custom_css()
+    if 'logged_in' not in st.session_state:
+        st.session_state['logged_in'] = False
 
-    menu = ["Upload Dataset", "Preprocessing", "Train Model", "Evaluate Model", "Diversity Report", "Predict Candidate"]
-    choice = st.sidebar.selectbox("Menu", menu)
+    if not st.session_state['logged_in']:
+        login()
+    else:
+        # Navigasi header
+        navigation = st.sidebar.selectbox("Navigasi", ["HOME", "Prediksi", "Laporan Keanekaragaman","Upload Dataset"])
 
-    if choice == "Upload Dataset":
-        st.subheader("Upload Dataset")
-        data = load_data()
+        if navigation == "HOME":
+            st.write("Selamat datang di Aplikasi Rekrutmen Tanpa Bias Gender!")
+        elif navigation == "Prediksi":
+            # Load data
+            data = load_data()
 
-    elif choice == "Preprocessing":
-        st.subheader("Preprocessing Data")
-        data = load_data()
-        processed_data = preprocess_data(data)
-        st.write("Data setelah preprocessing:")
-        st.write(processed_data.head(14))  # Show the first 14 rows after preprocessing
+            # Preprocessing data
+            data = preprocess_data(data)
 
-    elif choice == "Train Model":
-        st.subheader("Train Model")
-        data = load_data()
-        processed_data = preprocess_data(data)
-        X_train, X_test, y_train, y_test = split_data(processed_data)
-        model = train_model(X_train, y_train)
-        st.success("Model berhasil dilatih!")
+            # Split data
+            X_train, X_test, y_train, y_test = split_data(data)
 
-    elif choice == "Evaluate Model":
-        st.subheader("Evaluate Model")
-        data = load_data()
-        processed_data = preprocess_data(data)
-        X_train, X_test, y_train, y_test = split_data(processed_data)
-        model = train_model(X_train, y_train)
-        evaluate_model(model, X_test, y_test)
+            # Train model
+            model = train_model(X_train, y_train)
 
-    elif choice == "Diversity Report":
-        st.subheader("Diversity Report")
-        data = load_data()
-        gender_counts, education_counts, experience_counts, company_type_counts, company_size_counts, discipline_counts, last_new_job_counts, figures = generate_diversity_report(data)
-        pdf_file = export_report_to_pdf(data, gender_counts, education_counts, experience_counts, company_type_counts, company_size_counts, discipline_counts, last_new_job_counts, figures)
-        st.success(f"Laporan berhasil diekspor ke {pdf_file}")
-        pdf_base64 = file_to_base64(pdf_file)
-        href = f'<a href="data:application/octet-stream;base64,{pdf_base64}" download="{pdf_file}">Download Laporan PDF</a>'
-        st.markdown(href, unsafe_allow_html=True)
+            # Evaluate model
+            accuracy = evaluate_model(model, X_test, y_test)
+            st.write(f"Akurasi model: {accuracy * 100:.2f}%")
 
-    elif choice == "Predict Candidate":
-        st.subheader("Predict Candidate")
-        data = load_data()
-        processed_data = preprocess_data(data)
-        model = train_model(*split_data(processed_data)[:2])
+            # Menyimpan model setelah dilatih
+            if model is not None:
+                save_model(model)  # Simpan model setelah dilatih
 
-        st.write("Masukkan data kandidat:")
-        experience = st.slider("Experience (years)", min_value=0, max_value=25, value=0)
-        relevant_experience = st.selectbox("Relevant Experience", options=["No relevant experience", "Has relevant experience"])
-        enrolled_university = st.selectbox("Enrolled University", options=["no_enrollment", "Full time course", "Part time course"])
-        education_level = st.selectbox("Education Level", options=["Primary School", "High School", "Graduate", "Masters", "Phd"])
-        major_discipline = st.selectbox("Major Discipline", options=["STEM", "Business Degree", "Arts", "Humanities", "No Major", "Other"])
-        company_size = st.selectbox("Company Size", options=["<10", "10-49", "50-99", "100-500", "500-999", "1000-4999", "5000-9999", "10000+"])
-        company_type = st.selectbox("Company Type", options=["NGO", "Public Sector", "Pvt Ltd", "Early Stage Startup", "Funded Startup", "Other"])
-        last_new_job = st.selectbox("Last New Job", options=["never", "1", "2", "3", "4", "5+"])
+            # Menampilkan form input untuk memprediksi kelayakan kandidat
+            with st.sidebar:
+                st.markdown("<h1>Masukkan Biodata Kandidat</h1>", unsafe_allow_html=True)
 
-        candidate_data = pd.DataFrame({
-            "experience": [experience],
-            "relevent_experience": [relevant_experience],
-            "enrolled_university": [enrolled_university],
-            "education_level": [education_level],
-            "major_discipline": [major_discipline],
-            "company_size": [company_size],
-            "company_type": [company_type],
-            "last_new_job": [last_new_job]
-        })
+                enrollee_id = st.text_input("Enrollee ID", "")
+                city = st.text_input("City", "")
+                city_development_index = st.number_input("City Development Index", value=0.000, format="%.3f")
+                gender = st.selectbox("Gender", ["Male", "Female", "Other"])
+                relevent_experience = st.selectbox("Relevent Experience", ["Has relevent experience", "No relevent experience"])
+                enrolled_university = st.selectbox("Enrolled University", ["no_enrollment", "Full time course", "Part time course"])
+                education_level = st.selectbox("Education Level", ["Graduate", "Masters", "Phd"])
+                major_discipline = st.selectbox("Major Discipline", ["STEM", "Business Degree", "Arts", "No Major", "Other"])
+                experience = st.number_input("Experience", value=0)
+                company_size = st.selectbox("Company Size", ["<10", "10-49", "50-99", "100-500", "500-999", "1000-4999", "5000-9999", "10000+"])
+                company_type = st.selectbox("Company Type", ["Pvt Ltd", "Funded Startup", "Public Sector", "Early Stage Startup", "NGO", "Other"])
+                last_new_job = st.selectbox("Last New Job", ["never", "1", "2", "3", "4", ">4"])
+                training_hours = st.number_input("Training Hours", value=0)
 
-        label_encoder = LabelEncoder()
-        categorical_cols = ['relevent_experience', 'enrolled_university', 'education_level', 
-                            'major_discipline', 'company_size', 'company_type', 'last_new_job']
-        for col in categorical_cols:
-            candidate_data[col] = label_encoder.fit_transform(candidate_data[col])
+                # Tombol prediksi
+                prediksi_button = st.button("Prediksi")
 
-        prediction, probability = predict_candidate(model, candidate_data)
+                if prediksi_button:
+                    if (enrollee_id == "" or city == "" or gender == "" or relevent_experience == "" or
+                        enrolled_university == "" or education_level == "" or major_discipline == "" or
+                        experience == 0 or company_size == "" or company_type == "" or last_new_job == "" or
+                        training_hours == 0):
+                        st.error("Silakan isi semua form inputan terlebih dahulu!")
+                    else:
+                        # Menerapkan logika prediksi
+                        kelayakan = 0  # Initialize kelayakan to 0
+                        keterangan = 0  # Initialize keterangan to 0
+                        if (relevent_experience == "Has relevent experience" and
+                            (education_level == "Graduate" or education_level == "Masters" or education_level == "Phd") and
+                            major_discipline == "STEM" and  # Tambahkan syarat Major Discipline wajib STEM
+                            training_hours >= 50):
+                            kelayakan = 1
+                            keterangan = 1
 
-        prediction_data = {
-            "Experience": experience,
-            "Relevant Experience": relevant_experience,
-            "Enrolled University": enrolled_university,
-            "Education Level": education_level,
-            "Major Discipline": major_discipline,
-            "Company Size": company_size,
-            "Company Type": company_type,
-            "Last New Job": last_new_job,
-            "Prediksi": prediction,
-            "Probabilitas": f"{probability * 100:.2f}%",
-            "Keterangan": "Kandidat Diterima" if prediction == 1 else "Kandidat Ditolak"
-        }
+                        prediction_data = {
+                            "Enrollee ID": enrollee_id,
+                            "City": city,
+                            "City Development Index": city_development_index,
+                            "Gender": gender,
+                            "Relevent Experience": relevent_experience,
+                            "Enrolled University": enrolled_university,
+                            "Education Level": education_level,
+                            "Major Discipline": major_discipline,
+                            "Experience": experience,
+                            "Company Size": company_size,
+                            "Company Type": company_type,
+                            "Last New Job": last_new_job,
+                            "Training Hours": training_hours,
+                            "Keterangan": "Kandidat Diterima" if keterangan == 1 else "Kandidat Ditolak"
+                        }
 
-        st.write("Hasil Prediksi:")
-        for key, value in prediction_data.items():
-            st.write(f"{key}: {value}")
+                        if keterangan == 1:
+                            st.success("Kandidat Diterima!")
+                        else:
+                            st.error("Kandidat Ditolak!")
 
-        pdf_file = export_prediction_to_pdf(prediction_data)
-        st.success(f"Hasil prediksi berhasil diekspor ke {pdf_file}")
-        pdf_base64 = file_to_base64(pdf_file)
-        href = f'<a href="data:application/octet-stream;base64,{pdf_base64}" download="{pdf_file}">Download Hasil Prediksi PDF</a>'
-        st.markdown(href, unsafe_allow_html=True)
+                        # Export prediction results to PDF
+                        pdf_file = export_prediction_to_pdf(prediction_data)
+                        st.success("Hasil prediksi berhasil diekspor ke PDF!")
+                        download_file(pdf_file)
+
+        elif navigation == "Laporan Keanekaragaman":
+            data = load_data()
+            gender_counts, education_counts, experience_counts, company_type_counts, company_size_counts, discipline_counts, last_new_job_counts, figures = generate_diversity_report(data)
+            if st.button("Export Laporan ke PDF"):
+                pdf_file = export_report_to_pdf(data, gender_counts, education_counts, experience_counts, company_type_counts, company_size_counts, discipline_counts, last_new_job_counts, figures)
+                st.success("Laporan berhasil diekspor ke PDF!")
+                download_file(pdf_file)
+        
+        elif navigation == "Upload Dataset":
+            st.write("Upload Dataset")
+            # Upload file CSV
+            uploaded_file = st.file_uploader("Unggah file CSV dataset", type=["csv"])
+
+            if uploaded_file is not None:  # Check if file is uploaded
+                data = pd.read_csv(uploaded_file)  # Read the uploaded file directly
+                if validate_input(data):  # Call to validate_input
+                    data = preprocess_data(data)
+                    st.write("Dataset yang diunggah:")
+                    st.write(data.head(14))  # Display the uploaded dataset
+                    st.write(f"Jumlah data pada dataset: {len(data)}")  # Menambahkan informasi jumlah data
+                if data is not None:
+                    X_train, X_test, y_train, y_test = split_data(data)
+                    if X_train is not None:
+                        model = train_model(X_train, y_train)
+                        if model is not None:
+                            accuracy = evaluate_model(model, X_test, y_test)
+                            save_model(model)
+
+        # Tombol logout
+        logout()
 
 if __name__ == "__main__":
     main()
